dfaa5710b0f35bb195798d3bad31d6f873c98a98
Holm Hinrichs <holm.hinrichs@t-online.de>
2021-09-06 22:06:27 +0200

    adding Dockerfile

--- capstone/karma.conf.js
+++ capstone/karma.conf.js
@@ -20,7 +20,7 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    browsers: ['ChromeHeadlessCI'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
